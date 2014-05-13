bf95a7208ea4ffed0f060fdbb3761baeb025045f
huangVic <volcomvic@msn.com>
2014-05-14 01:08:49 +0800

    新增產品選單列表

---
+++ Controllers/FrontendController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopCar.Controllers
+{
+    public class FrontendController : Controller
+    {
+        //
+        // GET: /Frontend/
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ js/app/frontend.js
@@ -0,0 +1,31 @@
+﻿var frontendApp = angular.module('frontendApp', [], function ($locationProvider) {
+    $locationProvider.html5Mode(false);
+});
+
+frontendApp.$inject = ['$scope', '$filter', '$http'];
+
+/*****************************
+* Product Page controller
+*****************************/
+frontendApp.controller('productListCtrl', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
+    var po = Math.random()
+    console.log('po: ' + po);
+    $http({
+        method: 'GET',
+        url: '/Product/GetProClassList',
+        cache: false,
+        params: { _po: po },
+        headers: { 'Content-Type': 'application/json' } 
+    })
+       .success(function (data) {
+           console.log(data.classList);
+           $scope.classList = data.classList;
+
+       });
+}]);
+
+
+
+frontendApp.directive("customSort", function () {
+
+});
